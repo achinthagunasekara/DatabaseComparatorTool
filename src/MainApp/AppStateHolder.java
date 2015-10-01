f98d0b55872c911ee49b85f93ffd0722b7d95f29
Achintha Gunasekara <achintha@me.com>
2015-10-02 08:29:33 +1000

    Update AppStateHolder.java

--- src/MainApp/AppStateHolder.java
+++ src/MainApp/AppStateHolder.java
@@ -5,8 +5,8 @@ import DBTools.DBOperationManager;
 import java.sql.SQLException;
 
 /**
- * @author Archie Gunasekara
- * 2013.07.02
+ * @author: Archie Gunasekara
+ * @date: 2013.07.02
  */
 public class AppStateHolder {
     
